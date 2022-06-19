ee9fb2c23e8dcd8098083404b4c0bd132d35e030
Björn Erlwein <bjoern@bjoernerlwein.de>
2022-06-19 20:38:27 +0200

    feat: Allow triggering builds with ENTER

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,19 @@ use std::{
     path::Path,
     process::Command,
     sync::mpsc::channel,
+    thread,
     time::Duration,
 };
 
-use console::Term;
+use console::{Key, Term};
 use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
+
+static ENTER_HINT: &str = "You can trigger the command manually by pushing the <ENTER> key";
 
 fn main() {
     let stdin = io::stdin();
     let mut buffer = String::new();
-    let term = Term::stdout();
     // The channel will get the events
-    let (sender, receiver) = channel();
 
     let mut args = env::args();
     // Move past the command
@@ -28,13 +29,11 @@ fn main() {
             println!("\twatch-changes cargo build");
         }
         Some(command) => {
+            eprintln!("{}", ENTER_HINT);
             let mut command_args: Vec<String> = Vec::new();
             while let Some(cmd_arg) = args.next() {
                 command_args.push(cmd_arg);
             }
-
-            // The watcher will deliver debounced events once every second, should be plenty
-            let mut watcher = watcher(sender, Duration::from_secs(1)).unwrap();
 
             loop {
                 // Read in all lines from stdin
@@ -49,58 +48,95 @@ fn main() {
                 }
             }
 
-            buffer.split("\n").for_each(|file| {
-                let path = Path::new(file);
-                // The buffer will contain a final new line, skip that
-                if path.exists() {
-                    watcher
-                        .watch(path.to_str().unwrap(), RecursiveMode::NonRecursive)
-                        .expect(
-                            format!("Failed to set up a file watch for {}", path.display())
-                                .as_str(),
-                        );
+            let (command_sender, command_receiver) = channel();
+
+            let commandt = thread::spawn(move || {
+                let term = Term::stderr();
+                loop {
+                    command_receiver.recv().unwrap();
+                    term.clear_screen()
+                        .expect("Failed to clear the terminal screen");
+                    term.write_line(ENTER_HINT).unwrap();
+                    term.clear_line().unwrap();
+                    let output = Command::new(&command)
+                        .args(&command_args)
+                        .output()
+                        .expect("Failed to execute process");
+
+                    // Need to write line by line
+                    // the line write offsets will go out of sync with the terminal
+                    // and this needs to be fixed per line
+                    String::from_utf8_lossy(&output.stderr)
+                        .split("\n")
+                        .for_each(|line| {
+                            term.write_line(line).unwrap();
+                            // This seems to fix the line offsets screwing up
+                            term.clear_line().unwrap();
+                        });
                 }
             });
 
-            loop {
-                match receiver.recv() {
-                    Ok(event) => match event {
-                        // Watch the new path, stop watching the old
-                        DebouncedEvent::Rename(old, new) => {
-                            watcher.unwatch(old).unwrap();
-                            watcher.watch(new, RecursiveMode::NonRecursive).unwrap();
-                            term.clear_screen()
-                                .expect("Failed to clear the terminal screen");
-                            Command::new(&command)
-                                .args(&command_args)
-                                .spawn()
-                                .expect("Failed to execute process");
-                        }
-                        // Path no longer exists, no need to watch anymore
-                        DebouncedEvent::Remove(path) => {
-                            watcher.unwatch(path).unwrap();
-                            term.clear_screen()
-                                .expect("Failed to clear the terminal screen");
-                            Command::new(&command)
-                                .args(&command_args)
-                                .spawn()
-                                .expect("Failed to execute process");
-                        }
-                        // File was written, do X
-                        DebouncedEvent::Write(_path) => {
-                            term.clear_screen()
-                                .expect("Failed to clear the terminal screen");
-                            Command::new(&command)
-                                .args(&command_args)
-                                .spawn()
-                                .expect("Failed to execute process");
-                        }
-                        // Ignore all other events for now
-                        _ => (),
-                    },
-                    Err(e) => println!("watch error: {:?}", e),
+            let enter_command_sender = command_sender.clone();
+            let entert = thread::spawn(move || {
+                let command_sender = enter_command_sender;
+                let term = Term::stdout();
+                loop {
+                    let key = term.read_key().unwrap();
+                    if key == Key::Enter {
+                        command_sender.send(()).unwrap();
+                    }
                 }
-            }
+            });
+
+            let loop_command_sender = command_sender.clone();
+            let loopt = thread::spawn(move || {
+                let command_sender = loop_command_sender;
+                let (sender, receiver) = channel();
+                // The watcher will deliver debounced events once every second, should be plenty
+                let mut watcher = watcher(sender, Duration::from_secs(1)).unwrap();
+
+                buffer.split("\n").for_each(|file| {
+                    let path = Path::new(file);
+                    // The buffer will contain a final new line, skip that
+                    if path.exists() {
+                        watcher
+                            .watch(path.to_str().unwrap(), RecursiveMode::NonRecursive)
+                            .expect(
+                                format!("Failed to set up a file watch for {}", path.display())
+                                    .as_str(),
+                            );
+                    }
+                });
+
+                loop {
+                    match receiver.recv() {
+                        Ok(event) => match event {
+                            // Watch the new path, stop watching the old
+                            DebouncedEvent::Rename(old, new) => {
+                                watcher.unwatch(old).unwrap();
+                                watcher.watch(new, RecursiveMode::NonRecursive).unwrap();
+                                command_sender.send(()).unwrap();
+                            }
+                            // Path no longer exists, no need to watch anymore
+                            DebouncedEvent::Remove(path) => {
+                                watcher.unwatch(path).unwrap();
+                                command_sender.send(()).unwrap();
+                            }
+                            // File was written, do X
+                            DebouncedEvent::Write(_path) => {
+                                command_sender.send(()).unwrap();
+                            }
+                            // Ignore all other events for now
+                            _ => (),
+                        },
+                        Err(e) => println!("watch error: {:?}", e),
+                    }
+                }
+            });
+
+            loopt.join().unwrap();
+            commandt.join().unwrap();
+            entert.join().unwrap();
         }
     }
 }
