208c6944c2651f36fff6e5693d770f697bbf8866
Björn Erlwein <bjoern@bjoernerlwein.de>
2022-11-01 15:55:57 +0100

    update to new flake.nix

--- flake.nix
+++ flake.nix
@@ -1,26 +1,41 @@
 {
-  description = "virtual environments";
+  description = "Dev environment for rust";
 
-  inputs.devshell.url = "github:numtide/devshell";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/22.05";
+  inputs.devshell = {
+    url = "github:numtide/devshell";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+  inputs.rust-overlay = {
+    url = "github:oxalica/rust-overlay";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, flake-utils, devshell, fenix, nixpkgs }:
+  outputs = { self, flake-utils, devshell, nixpkgs, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system: {
-      defaultPackage = let pkgs = import nixpkgs { inherit system; };
-      in pkgs.rustPlatform.buildRustPackage {
-        pname = "watch-changes";
-        version = "0.3.0";
-        src = ./.;
-        cargoLock.lockFile = ./Cargo.lock;
-      };
-      devShell = let
-        pkgs = import nixpkgs {
-          inherit system;
-
-          overlays = [ devshell.overlay ];
+      defaultPackage =
+        let pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.rustPlatform.buildRustPackage {
+          pname = "watch-changes";
+          version = "0.3.1";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
         };
-      in pkgs.devshell.mkShell {
-        imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
-      };
+      devShell =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+
+            overlays = [ devshell.overlay rust-overlay.overlays.default ];
+          };
+        in
+        pkgs.devshell.mkShell {
+          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+        };
     });
 }
